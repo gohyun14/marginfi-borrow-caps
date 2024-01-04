7e0a808bae4244f420f67ec5c6e0910a4594f13e
Gabe Hyun <gabe.hyun@gmail.com>
2024-01-03 18:30:59 -0800

    added simple nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,7 +21,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body className={`font-sans ${inter.variable}`}>
+        <nav className="flex w-full flex-row items-center justify-center divide-x bg-zinc-900 px-3 py-2 text-zinc-200">
+          <Link href="/" className="text-sm px-2">
+            Borrow Caps
+          </Link>
+
+          <Link href="/health" className="text-sm px-2">
+            Health Check
+          </Link>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
