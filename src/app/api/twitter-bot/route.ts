b8051bcca5f3f08d44acec75558368f1354e7edf
Gabe Hyun <gabe.hyun@gmail.com>
2024-01-17 18:42:59 -0500

    please

--- src/app/api/twitter-bot/route.ts
+++ src/app/api/twitter-bot/route.ts
@@ -1,10 +1,4 @@
-import { MarginfiClient, getConfig } from "@mrgnlabs/marginfi-client-v2";
-import { Connection, PublicKey } from "@solana/web3.js";
-import { NextResponse } from "next/server";
-import { type Account, type TokenMetadata } from "~/lib/types";
-
-export async function GET() {
-  console.log("server");
+export async function GET(request: Request) {
   const response = new Response(JSON.stringify({ message: "success" }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
