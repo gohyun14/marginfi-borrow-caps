dc86f27b8f8cc1a8e37d1b4aec11b11e28d2d633
Gabe Hyun <gabe.hyun@gmail.com>
2024-01-17 19:03:03 -0500

    working i hope

--- src/app/twitter-bot/page.tsx
+++ src/app/twitter-bot/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-export default function TwitterBot() {
-  // const data = await fetch("", {
-  //   method: "GET",
-  // });
-  // console.log("component", data);
-  return <div>TwitterBot</div>;
+export default async function TwitterBot() {
+  const data = await fetch(
+    `${process.env.API_URL}/twitter-bot?address=4yhXa4iERFGma3T1HMMjH8nJ8EdFqbahCV5Yomm8Z3do`,
+    {
+      method: "GET",
+    },
+  ).then((res) => res.json() as Promise<{ address: string }>);
+  console.log("component", data);
+  return <div>{data.address}</div>;
 }
